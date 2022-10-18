5feb7783e0fee6c7540f13f7028a48db6a371c66
SalhSthai <firstheart00@gmail.com>
2022-10-18 11:40:36 +0700

    test

--- src/features/LoginWithGoogle.js
+++ src/features/LoginWithGoogle.js
@@ -1,60 +1,60 @@
-import React, { useEffect, useState } from 'react';
-import { GoogleLogin, GoogleLogout } from 'react-google-login';
-import { gapi } from 'gapi-script';
+// import React, { useEffect, useState } from 'react';
+// import { GoogleLogin, GoogleLogout } from 'react-google-login';
+// import { gapi } from 'gapi-script';
 
-export default function LoginWithGoogle() {
-  const clientId =
-    '939537041811-u32sb2h2l769u5jcfg55ljq6jh9i9huv.apps.googleusercontent.com';
+// export default function LoginWithGoogle() {
+//   const clientId =
+//     '939537041811-u32sb2h2l769u5jcfg55ljq6jh9i9huv.apps.googleusercontent.com';
 
-  const [profile, setProfile] = useState(null);
-  useEffect(() => {
-    const initClient = () => {
-      gapi.client.init({
-        clientId: clientId,
-        scope: ''
-      });
-    };
-    gapi.load('client:auth2', initClient);
-  }, []);
+//   const [profile, setProfile] = useState(null);
+//   useEffect(() => {
+//     const initClient = () => {
+//       gapi.client.init({
+//         clientId: clientId,
+//         scope: ''
+//       });
+//     };
+//     gapi.load('client:auth2', initClient);
+//   }, []);
 
-  const onSuccess = (res) => {
-    setProfile(res.profileObj);
-    console.log('success', res);
-  };
-  const onFaliure = (res) => {
-    console.log('falied', res);
-  };
+//   const onSuccess = (res) => {
+//     setProfile(res.profileObj);
+//     console.log('success', res);
+//   };
+//   const onFaliure = (res) => {
+//     console.log('falied', res);
+//   };
 
-  const logOut = () => {
-    setProfile(null);
-  };
+//   const logOut = () => {
+//     setProfile(null);
+//   };
 
-  return (
-    <div>
-      {profile ? (
-        <div>
-          <img src={profile.imageUrl} alt="user image" />
-          <h3>User Logged In</h3>
-          <p>Name: {profile.name}</p>
-          <p>Email: {profile.email}</p>
-          <br />
-          <br />
-          <GoogleLogout
-            clientId={clientId}
-            buttonText="Log out"
-            onLogoutSuccess={logOut}
-          />
-        </div>
-      ) : (
-        <GoogleLogin
-          clientId={clientId}
-          buttonText="Sign in with google"
-          onSuccess={onSuccess}
-          onFailure={onFaliure}
-          cookiePolicy={'single_host_origin'}
-          isSignedIn={true}
-        />
-      )}
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       {profile ? (
+//         <div>
+//           <img src={profile.imageUrl} alt="user image" />
+//           <h3>User Logged In</h3>
+//           <p>Name: {profile.name}</p>
+//           <p>Email: {profile.email}</p>
+//           <br />
+//           <br />
+//           <GoogleLogout
+//             clientId={clientId}
+//             buttonText="Log out"
+//             onLogoutSuccess={logOut}
+//           />
+//         </div>
+//       ) : (
+//         <GoogleLogin
+//           clientId={clientId}
+//           buttonText="Sign in with google"
+//           onSuccess={onSuccess}
+//           onFailure={onFaliure}
+//           cookiePolicy={'single_host_origin'}
+//           isSignedIn={true}
+//         />
+//       )}
+//     </div>
+//   );
+// }
--- src/redux/ReduxProvider.js
+++ src/redux/ReduxProvider.js
@@ -1,8 +1,20 @@
-import { Provider } from 'react-redux';
+import { useEffect } from 'react';
+import { Provider, useDispatch, useSelector } from 'react-redux';
 import {store} from './index';
+import { getAccessToken } from '../utils/localStorage';
+import { thunkRemember } from './Slice/AuthSlice';
+
 
 export default function ({ children }) {
-  
+  const state = useSelector((state) => state);
+  const dispatch = useDispatch();
+  useEffect(()=>{
+    getAccessToken() && dispatch(thunkRemember(getAccessToken()));
+
+  },[])
+
+
+
   return (
     <Provider store={store}>{children}</Provider>
   )
