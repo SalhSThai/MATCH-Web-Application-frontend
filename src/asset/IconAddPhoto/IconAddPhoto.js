a97a5f296a8c51ad1de777208ad076c06e20bd61
chadaniss <job.chadanis@gmail.com>
2022-10-28 14:39:10 +0700

    fixed get my location (swipe page)

--- src/asset/IconAddPhoto/IconAddPhoto.js
+++ src/asset/IconAddPhoto/IconAddPhoto.js
@@ -8,10 +8,10 @@ function IconAddPhoto() {
       width='24'
       height='24'
       viewBox='0 0 24 24'
-      stroke-width='2'
+      strokeWidth='2'
       stroke='currentColor'
       fill='none'
-      stroke-linecap='round'
+      strokeLinecap='round'
       stroke-linejoin='round'
     >
       <path stroke='none' d='M0 0h24v24H0z' fill='none'></path>
