f8c945eaf2bf05318f451b4a8ee240cd4198241a
Basbusyy <Pasit.Jeamrattanasakul@gmail.com>
2022-10-31 15:28:00 +0700

    feature:ToggleLikes-Success

--- src/pages/ExplorePage.js
+++ src/pages/ExplorePage.js
@@ -1,23 +1,18 @@
-import InterestCard from '../components/exploreComponents/InterestCard';
+
+
+import ExploreBody from '../components/exploreComponents/ExploreBody';
 import NearMeButton from '../components/exploreComponents/NearMeButton';
 
 export default function ExplorePage() {
   return (
     <>
-      <div className='flex justify-between w-full px-5'>
-        <div className='py-2 font-semibold'>Suggest for you</div>
+      <div className="flex justify-between w-full px-5">
+        <div className="py-2 font-semibold">Suggest for you</div>
         <NearMeButton />
       </div>
-      <div className='grid grid-cols-2 grid-flow-row gap-3 p-3 max-h-full overflow-y-scroll scrollbar-hide'>
-        <InterestCard />
-        <InterestCard />
-        <InterestCard />
-        <InterestCard />
-        <InterestCard />
-        <InterestCard />
-        <InterestCard />
-        <InterestCard />
+      <div className="max-h-full overflow-y-scroll scrollbar-hide">
+        <ExploreBody />
       </div>
     </>
   );
-}
+}	
